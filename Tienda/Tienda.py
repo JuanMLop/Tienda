275bddcc5b515b69349752f9e184dc6b7c598691
Juan.Mlop <137979666+JuanMLop@users.noreply.github.com>
2024-04-04 12:50:38 -0500

    Update Tienda.py

--- Tienda/Tienda.py
+++ Tienda/Tienda.py
@@ -1,3 +1,4 @@
+from typing import Any
 from Producto import Producto
 class Tienda:
     """---------------------------------------------------------------------------
@@ -6,12 +7,28 @@ class Tienda:
     ---------------------------------------------------------------------------"""
 
 
-    __producto1 = None
-    __producto2 = None
-    __producto3 = None
-    __producto4 = None
+    #__producto1 = None
+    #__producto2 = None
+    #__producto3 = None
+    #__producto4 = None
  
- 
+    """---------------------------------------------------------------------------
+    #Metodos
+    ---------------------------------------------------------------------------"""
+    
+    def __init__(self, ):
+        self.dineroEnCaja= 0
+        self.__producto1= Producto("Papeleria", "Lapiz", 550.0, 18, 5)
+        self.__producto2= Producto("Papeleria", "Borrador", 300.0, 15, 5)
+        self.__producto3= Producto("Supermercado", "Cafe", 5600.0, 20, 10)
+        self.__producto4= Producto("Drogueria", "Desinfectante", 3200.0, 12, 11)
+    
     def getProducto1(self):
         return self.__producto1
+    def getProducto2(self):
+        return self.__producto2
+    def getProducto3(self):
+        return self.__producto3
+    def getProducto4(self):
+        return self.__producto4
         
